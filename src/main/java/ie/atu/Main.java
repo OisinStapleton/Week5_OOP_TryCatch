992c7f214a497672f7a64ae6ac8d44d3ed126db6
OisinStapleton-STUDE <G00365445@atu.ie>
2025-10-20 09:40:05 +0100

    Adding trycatch feature

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,19 +1,20 @@
 package ie.atu;
+import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+        int age = 0;
+        while (true) {
+            System.out.print("Enter your age: ");
+            String text = sc.nextLine().trim();
+            try {
+                age = Integer.parseInt(text);
+                System.out.println("Thanks! You entered age = " + age);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Input" + e.getMessage());
+            }
         }
     }
 }
